c82e39b84b9f09e1b95eb85d6fb0e378d789b436
Ike <ikescode@gmail.com>
2020-06-25 01:26:46 -0500

    add build to master for production machines

---
+++ build/precache-manifest.2e1f66dac26d979380d18ed118f9768c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6bf2f87e08dedc1cc7968322f7050ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44799128cab093e353ed",
+    "url": "/static/css/main.015d3aaa.chunk.css"
+  },
+  {
+    "revision": "6aa9b86e8128724404c6",
+    "url": "/static/js/2.744c2bc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.744c2bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44799128cab093e353ed",
+    "url": "/static/js/main.6fb1de97.chunk.js"
+  },
+  {
+    "revision": "2875c459b986d5fd3bb6",
+    "url": "/static/js/runtime-main.354c6194.js"
+  },
+  {
+    "revision": "d6cb6efd46fbda6488dbaabb6027b8a9",
+    "url": "/static/media/2.d6cb6efd.mp3"
+  },
+  {
+    "revision": "fb716f416c6dfea68f9be1293692773e",
+    "url": "/static/media/3.fb716f41.mp3"
+  },
+  {
+    "revision": "e549b76bcd3361e26b252eb5f18c4688",
+    "url": "/static/media/4.e549b76b.mp3"
+  },
+  {
+    "revision": "035e3f2d238d7c72d2264873abec12b8",
+    "url": "/static/media/5.035e3f2d.mp3"
+  },
+  {
+    "revision": "36927f3bfc6286269abf14a02aaf93df",
+    "url": "/static/media/6.36927f3b.mp3"
+  },
+  {
+    "revision": "be46cf9662a89102c4d2671d4b683909",
+    "url": "/static/media/bg.be46cf96.jpg"
+  },
+  {
+    "revision": "be482a2aa6ba3acdc12f0e0e191ff46e",
+    "url": "/static/media/logo.be482a2a.png"
+  }
+]);
